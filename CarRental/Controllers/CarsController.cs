5b1e85a52ba4c8d11f49bbc64f33891a93898d58
LachezarIvanov-git <118828142+LachezarIvanov-git@users.noreply.github.com>
2023-02-12 17:29:16 +0200

    workings on authorization

--- CarRental/Controllers/CarsController.cs
+++ CarRental/Controllers/CarsController.cs
@@ -1,38 +1,41 @@
 ﻿using CarRental.Data;
 using CarRental.Data.Models;
+using CarRental.InfraStructure;
 using CarRental.Models.Cars;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using FileSystem=System.IO.File;
 
 namespace CarRental.Controllers
 {
-    public class CarsController:Controller
+    public class CarsController : Controller
     {
         public readonly CarRentalDbContext data;
 
         public CarsController(CarRentalDbContext data)
-            =>this.data = data;
+            => this.data = data;
 
-        public IActionResult All([FromQuery]CarQueryModel query)
+        public IActionResult All([FromQuery] CarQueryModel query)
         {
             var carsQuery = this.data.Cars.AsQueryable();
-            
+
             if (!string.IsNullOrEmpty(query.Brand))
             {
                 carsQuery = carsQuery
                     .Where(x => x.Brand == query.Brand);
-                    
+
             }
 
 
             if (!string.IsNullOrEmpty(query.SearchTerm))
             {
                 carsQuery = carsQuery.Where(x =>
-                x.Brand.ToLower().Contains(query.SearchTerm.ToLower())||
-                x.Model.ToLower().Contains(query.SearchTerm.ToLower())||
+                x.Brand.ToLower().Contains(query.SearchTerm.ToLower()) ||
+                x.Model.ToLower().Contains(query.SearchTerm.ToLower()) ||
                 x.Description.ToLower().Contains(query.SearchTerm.ToLower())
                 );
             }
@@ -49,41 +52,47 @@ namespace CarRental.Controllers
                 .Skip((query.CurrentPage - 1) * CarQueryModel.CarsPerPage)
                 .Take(CarQueryModel.CarsPerPage)
                 //.OrderByDescending(x => x.Id)
-                .Select(x=> new CarListingViewModel
+                .Select(x => new CarListingViewModel
                 {
-                Id= x.Id,
-                Brand=x.Brand,
-                Model=x.Model,
-                ImgUrl=x.ImageUrl,
-                Category=x.Category.Name,
-                Year=x.Year,
+                    Id = x.Id,
+                    Brand = x.Brand,
+                    Model = x.Model,
+                    ImgUrl = x.ImageUrl,
+                    Category = x.Category.Name,
+                    Year = x.Year,
                 })
                 .ToList();
 
-            
 
-            var carBrands=this.data
+
+            var carBrands = this.data
                 .Cars
-                .Select(c=>c.Brand)
+                .Select(c => c.Brand)
                 .Distinct()
-                .OrderBy(c=>c)
+                .OrderBy(c => c)
                 .ToList();
-            
-            
 
-            query.Brands= carBrands;
+
+
+            query.Brands = carBrands;
             query.Cars = cars;
 
-            
+
 
             return View(query);
         }
-
-        public IActionResult Add() => View(new AddCarrFormModel
+        [Authorize]
+        public IActionResult Add()
         {
-            Categories = this.GetCarCategories()
-        });
+            var UserId = this.User.GetId();
+            var userIsDealer=this.data.Dealers.Any(d => d.UserId == UserId);
 
+        return View(new AddCarrFormModel {
+                Categories = this.GetCarCategories()
+                   });
+        }
+
+        [Authorize]
         [HttpPost]
         public IActionResult Add(AddCarrFormModel car, IFormFile image)
         {
@@ -118,6 +127,11 @@ namespace CarRental.Controllers
 
             return RedirectToAction(nameof(All));
         }
+
+        private bool UserIsDealer()
+            => !this.data
+            .Dealers
+            .Any(d => d.UserId == this.User.GetId());
 
         private IEnumerable<CarCategoryViewModel> GetCarCategories()
             => this.data
---
+++ CarRental/InfraStructure/ClaimsPrincipalExtentions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace CarRental.InfraStructure
+{
+    public static class ClaimsPrincipalExtentions
+    {
+        public static string GetId(this ClaimsPrincipal user)=>
+            user.FindFirst(ClaimTypes.NameIdentifier).Value;
+    }
+}
